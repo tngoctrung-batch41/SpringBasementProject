c3ff76a6a15205ff66fb6c78b7a67d9d8f0bfe6f
tngoctrung-batch41 <tngoctrung-batch41@sdc.edu.vn>
2023-08-03 14:13:38 +0700

    basement

---
+++ src/main/java/com/project/Myschedule/MyscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.project.Myschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyscheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/Myschedule/MyscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Myschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
